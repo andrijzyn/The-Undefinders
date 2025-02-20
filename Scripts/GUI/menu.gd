14b37efa32f1dfbc6123bbce9d1bdfd7c8a4aa8e
stepnoff <playingthird@gmail.com>
2025-02-20 10:34:21 +0200

    Remade folder structure
    + Scenes/GUI folder
    + Scenes/Static Nodes folder
    + Scenes/Character Nodes folder
    + Scripts/GUI folder
    + Assets/Font folder

--- Scripts/GUI/menu.gd
+++ Scripts/GUI/menu.gd
@@ -5,7 +5,7 @@ func play() -> void:
 	get_tree().change_scene_to_file("res://Scenes/main_scene.tscn")
 
 func options() -> void:
-	get_tree().change_scene_to_file("res://Scenes/options.tscn")
+	get_tree().change_scene_to_file("res://Scenes/GUI/options.tscn")
 
 func exit() -> void:
 	get_tree().quit()
