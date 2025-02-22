ceca13665840e5fc6d88d4ba8d882a6a43d9b9b4
Zyniuk Andrij <62100293+andrijzyn@users.noreply.github.com>
2025-02-22 14:12:15 +0200

    Moved magic numbers to const

--- Scripts/Classes/RaycastHandler.gd
+++ Scripts/Classes/RaycastHandler.gd
@@ -1,22 +1,23 @@
 class_name RaycastHandler
 
+const RAY_LENGTH := 1000  # Константа длины луча
+
 static func getRaycastResult(camera: MainCamera) -> Node3D:
 	var space_state := camera.get_world_3d().direct_space_state
 	var mouse_pos := camera.get_viewport().get_mouse_position()
 	var from := camera.project_ray_origin(mouse_pos)
-	var to := from + camera.project_ray_normal(mouse_pos) * 1000  # Длина луча
+	var to := from + camera.project_ray_normal(mouse_pos) * RAY_LENGTH
 	var query := PhysicsRayQueryParameters3D.create(from, to)
 	var result := space_state.intersect_ray(query)
-	if result: return result.collider
-	else: return null
 	
+	return result ? result.collider : null
+
 static func getRaycastResultPosition(camera: MainCamera) -> Vector3:
 	var space_state := camera.get_world_3d().direct_space_state
 	var mouse_pos := camera.get_viewport().get_mouse_position()
 	var from := camera.project_ray_origin(mouse_pos)
-	var to := from + camera.project_ray_normal(mouse_pos) * 1000  # Длина луча
+	var to := from + camera.project_ray_normal(mouse_pos) * RAY_LENGTH
 	var query := PhysicsRayQueryParameters3D.create(from, to)
 	var result := space_state.intersect_ray(query)
-	if result: return result.position
-	else: return Vector3.ZERO
 	
+	return result ? result.position : Vector3.ZERO
