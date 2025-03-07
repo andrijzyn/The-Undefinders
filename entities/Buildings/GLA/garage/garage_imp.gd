89b1947e2eb1b95c022cba3f13957c6e78f94aae
RealMasket <166716449+RealMasket@users.noreply.github.com>
2025-03-07 19:53:16 +0200

    Refactored production buildings into a modular system
    - Created a `ProductionBuilding` base class to handle production queues, UI updates, and production logic.
    - Moved common logic (queue management, production processing, cancellation) into `ProductionBuilding`.
    - `GarageImp` now extends `ProductionBuilding`, keeping only garage-specific logic (door animations and unit spawning).
    - Improved code reusability, allowing easy creation of new production buildings with different behaviors.

--- entities/Buildings/GLA/garage/garage_imp.gd
+++ entities/Buildings/GLA/garage/garage_imp.gd
@@ -1,43 +1,29 @@
-extends Node3D
+extends ProductionBuilding
 class_name GarageImp
 
-var available_items = {
-	"Soldier/soldier": { "icon": preload("res://entities/Units/USA/Infantry/Soldier/soldier_icon.PNG"), "cost": 100, "time": 5.0}
-}
-
-var ui
-var icon: Texture2D = preload("res://features/GUI/textures/barracks.png")
-var is_builder: bool = false
-var isSelected: bool = false;
-var is_garage_active: bool = false;
+var is_garage_active: bool = false
 @onready var animPlayer := $AnimationPlayer
 @onready var spawn_point := $SpawnPoint
 @onready var exit_point := $ExitPoint
 
-var production_queue: Array = []
-var is_producing: bool = false
-const MAX_QUEUE_SIZE: int = 10
-
-func _init() -> void:
-	add_to_group(Constants.selectable)
-	add_to_group(Constants.mouseChanger)
-
-func _ready() -> void:
-	ui = get_tree().get_root().get_node("MainScene/RTS_UI")
+func _ready():
+	super()
+	available_items = {
+		"Soldier/soldier": { "icon": preload("res://entities/Units/USA/Infantry/Soldier/soldier_icon.PNG"), "cost": 100, "time": 5.0}
+	}
+	icon = preload("res://features/GUI/textures/barracks.png")
 	animPlayer.animation_finished.connect(_on_animation_finished)
 
-func setSelected(val:bool):
+func setSelected(val: bool):
 	if is_garage_active:
 		return true
-	isSelected = val
-	if isSelected == true:
+	super(val)
+	if isSelected:
 		animPlayer.play("Selected")
-	else: animPlayer.stop()
+	else:
+		animPlayer.stop()
 
-func get_available_items() -> Dictionary:
-	return available_items 
-
-func spawn_unit(unit_name: String):
+func on_production_complete(unit_name: String):
 	is_garage_active = true
 	animPlayer.play("GarageDoorOpening")
 	await animPlayer.animation_finished
@@ -47,64 +33,10 @@ func spawn_unit(unit_name: String):
 	get_parent().add_child(unit_instance)
 	unit_instance.global_transform.origin = spawn_point.global_transform.origin
 	unit_instance.move_to_exit_point(exit_point.global_transform.origin)
-	
+
 	await unit_instance.reached_exit
 	animPlayer.play("GarageDoorClose")
 	await animPlayer.animation_finished
-
-func start_production(unit_name: String, unit_icon: Texture2D):
-	if production_queue.size() < MAX_QUEUE_SIZE:
-		var production_time = available_items[unit_name].get("time", 5.0)
-		production_queue.append({
-			"name": unit_name,
-			"icon": unit_icon,
-			"progress": 0.0,
-			"production_time": production_time
-		})
-		ui.add_to_production(self, unit_icon)
-		if not is_producing:
-			process_next_in_queue()
-
-func process_next_in_queue():
-	if production_queue.is_empty():
-		is_producing = false
-		return
-
-	is_producing = true
-	var current_product = production_queue[0]
-	var progress = current_product.get("progress", 0.0)
-	var production_time = current_product["production_time"]
-	for i in range(int(progress * 100), 100):
-		await get_tree().create_timer(production_time / 100.0).timeout
-		if production_queue.is_empty() or production_queue[0] != current_product:
-			is_producing = false
-			if not production_queue.is_empty():
-				process_next_in_queue()
-			return
-		current_product["progress"] = (i + 1) / 100.0
-		ui.update_production_progress(self, current_product["progress"])
-
-	spawn_unit(current_product["name"])
-	production_queue.pop_front()
-	ui.complete_production(self)
-
-	process_next_in_queue()
-
-func cancel_production(index: int):
-	if index >= 0 and index < production_queue.size():
-		var progress_to_transfer = 0.0
-		if index == 0 and is_producing and production_queue.size() > 1:
-			if production_queue[1]["name"] == production_queue[0]["name"]:
-				progress_to_transfer = production_queue[0]["progress"]
-		production_queue.remove_at(index)
-
-		if progress_to_transfer > 0.0 and not production_queue.is_empty():
-			production_queue[0]["progress"] = progress_to_transfer
-		if index == 0 and is_producing:
-			is_producing = false
-			process_next_in_queue()
-
-		ui.update_production_queue()
 
 func _on_animation_finished(anim_name: String):
 	if anim_name == "GarageDoorClose":
